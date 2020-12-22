31187dc8cd79800803e612da2e4c364df79894db
John Wayne Carreon <55916874+jwayne03@users.noreply.github.com>
2020-12-22 19:20:45 +0100

    #4 Resolve the map charge in SplashActivity

--- app/src/main/java/com/example/stubar/MapsActivity.java
+++ app/src/main/java/com/example/stubar/MapsActivity.java
@@ -2,7 +2,11 @@ package com.example.stubar;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,18 +15,57 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private SearchView searchView;
+    private SupportMapFragment supportMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
+        searchView = findViewById(R.id.svlocation);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+
+                if (location != null || !location.equals("")) {
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                    Toast.makeText(getApplicationContext(), address.getLatitude()
+                            + " " + address.getLongitude(), Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        supportMapFragment.getMapAsync(this);
     }
 
     /**
@@ -38,9 +81,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng barcelona = new LatLng(41.390205, 2.154007);
-        mMap.addMarker(new MarkerOptions().position(barcelona).title("Barcelona"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(barcelona));
+//         Add a marker in Sydney and move the camera
+//        LatLng barcelona = new LatLng(41.390205, 2.154007);
+//        mMap.addMarker(new MarkerOptions().position(barcelona).title("Barcelona"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(barcelona));
     }
 }
