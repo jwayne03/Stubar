5364a23651430a1a881c2c26e32d39e77d34bfc6
wayne <johnwayne.carreon@gmail.com>
2020-12-22 15:59:34 +0100

    #3 added google maps activity and implemented API key

--- app/src/main/java/com/example/stubar/SplashActivity.java
+++ app/src/main/java/com/example/stubar/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.stubar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
