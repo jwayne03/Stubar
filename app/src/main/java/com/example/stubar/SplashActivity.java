19accac57a864fe8688c119dc2dc1c32232fa6f4
wayne <johnwayne.carreon@gmail.com>
2020-12-24 18:44:38 +0100

    #7 Added ProgressBar in Splash Activity

--- app/src/main/java/com/example/stubar/SplashActivity.java
+++ app/src/main/java/com/example/stubar/SplashActivity.java
@@ -4,15 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements Runnable {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Program and fire the handler immediately
+        handler = new Handler();
+        handler.postDelayed(this, 2000);
+    }
+
+    @Override
+    public void run() {
         Intent intent = new Intent(SplashActivity.this, MapsActivity.class);
+        finish();
         startActivity(intent);
     }
 }
