f4a35c9563ce875a8992a6b7ea1a6ddbd486fa27
nadiflexx <nadi-1999@hotmail.es>
2021-01-07 13:03:58 +0100

    #16.2 [REGISTER] Create the register activity.

---
+++ app/src/main/java/com/example/stubar/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.stubar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.SignInButton;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
